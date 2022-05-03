16b1040f74663d58bb05c36904a333e157cca34c
munvei <roots19981108@gmail.com>
2022-05-03 22:50:39 +0900

    Google Calendar API のサンプルコード イベントの確認と予定の追加ができる

---
+++ api-test/show-events.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+secret_path = '../secret/'
+token_path = secret_path + 'token.json'
+cred_path = secret_path + 'credentials.json'
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                cred_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_path, 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        # イベントの追加
+        event = {
+            # title
+            'summary': '履歴書',
+            # 開始時間
+            'start': {
+                'dateTime': datetime.datetime(2022, 5, 9, 9, 0).isoformat(),
+                'timeZone': 'Japan'
+            },
+            'end': {
+                'dateTime': datetime.datetime(2022, 5, 9, 10, 0).isoformat(),
+                'timeZone': 'Japan'
+            },
+        }
+        service.events().insert(calendarId='yoichi.nagamoto@gmail.com', body=event).execute() 
+
+        # 今後のイベント10件表示
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
